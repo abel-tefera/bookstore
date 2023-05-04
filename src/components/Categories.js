addeb8fd121681a76b0fee4dfc8ba2d49c0c2d47
abel-tefera <abeltefera16@gmail.com>
2023-05-04 17:44:53 +0300

    Created the categories page

---
+++ src/components/Categories.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Categories = () => {
+  return (
+    <div className="container px-1 md:px-12">
+      <div>Categories</div>
+    </div>
+  );
+};
+
+export default Categories;
