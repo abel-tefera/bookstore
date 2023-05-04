381b7d9a400193d445d35cec07627815ce5cc71d
abel-tefera <abeltefera16@gmail.com>
2023-05-04 18:25:43 +0300

    Refactored form into a component

---
+++ src/components/AddBookForm.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AddBookForm = () => {
+  return (
+    <form className="flex flex-col">
+      <div className="text-xl">Add New Book</div>
+      <div className="flex flex-row">
+        <input type="text" placeholder="Book Title" />
+        <input type="text" placeholder="Author" />
+        <button type="submit">Add Book</button>
+      </div>
+    </form>
+  );
+};
+
+export default AddBookForm;
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import books from '../data/books';
 import Book from './Book';
+import AddBookForm from './AddBookForm';
 
 const Books = () => (
   <div className="container px-1 md:px-12 flex flex-col">
     {books.map(({ title, category, author }, i) => (
       <Book key={i} title={title} category={category} author={author} />
     ))}
-    <form className="flex flex-col">
-      <div className="text-xl">Add New Book</div>
-      <div className="flex flex-row">
-        <input type="text" placeholder="Book Title" />
-        <input type="text" placeholder="Author" />
-        <button type="submit">Add Book</button>
-      </div>
-    </form>
+
+    <AddBookForm />
   </div>
 );
 
