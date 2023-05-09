b7f9ec50a27dcb0797bb6567189957afcbc8b3d9
abel-tefera <abeltefera16@gmail.com>
2023-05-09 18:53:06 +0300

    Added books list to component

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import books from '../data/books';
+import { useSelector, useDispatch } from 'react-redux';
+// import books from '../data/books';
 import Book from './Book';
 import AddBookForm from './AddBookForm';
 
-const Books = () => (
-  <div className="container px-1 md:px-12 flex flex-col">
-    {books.map(({ title, category, author }, i) => (
-      <Book key={i} title={title} category={category} author={author} />
-    ))}
+const Books = () => {
+  const books = useSelector((state) => state.books.booksArr);
+  return (
+    <div className="container px-1 md:px-12 flex flex-col">
+      {books.map(({
+        item_id, title, category, author,
+      }, i) => (
+        <Book key={i} item_id={item_id} title={title} category={category} author={author} />
+      ))}
 
-    <AddBookForm />
-  </div>
-);
+      <AddBookForm />
+    </div>
+  );
+};
 
 export default Books;
