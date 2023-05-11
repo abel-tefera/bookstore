9c807e687b68e8a9eeaf0a2e80d9916ce6e3af19
abel-tefera <abeltefera16@gmail.com>
2023-05-11 14:54:40 +0300

    Displayed books from api

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,21 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 // import books from '../data/books';
 import Book from './Book';
 import AddBookForm from './AddBookForm';
+import {
+  getBooksError,
+  getBooksStatus,
+  selectAllBooks,
+  fetchBooks,
+} from '../redux/books/booksSlice';
 
 const Books = () => {
-  const books = useSelector((state) => state.books.booksArr);
-  return (
+  const dispatch = useDispatch();
+
+  const books = useSelector(selectAllBooks);
+  const status = useSelector(getBooksStatus);
+  const error = useSelector(getBooksError);
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchBooks());
+    }
+  }, [status, dispatch]);
+
+  return status === 'succeeded' ? (
     <div className="container px-1 md:px-12 flex flex-col">
       {books.map(({
         item_id, title, category, author,
       }, i) => (
-        <Book key={i} item_id={item_id} title={title} category={category} author={author} />
+        <Book
+          key={i}
+          item_id={item_id}
+          title={title}
+          category={category}
+          author={author}
+        />
       ))}
 
       <AddBookForm />
     </div>
+  ) : status === 'failed' ? (
+    <div>
+      Error:
+      {' '}
+      {error}
+    </div>
+  ) : (
+    <div>Loading...</div>
   );
 };
 
